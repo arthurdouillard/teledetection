f44f8207dd50a63e901e59166d0e8f379e59f6b7
Arthur Douillard <douill_a@epita.fr>
2017-12-13 15:25:58 +0100

    Add update of new mass centers.

--- NUAGES_5A/SRC/compute.c
+++ NUAGES_5A/SRC/compute.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <math.h>
 #include <float.h>
+#include <string.h>
 #include <gtk/gtk.h>
 
 #include "compute.h"
@@ -45,14 +46,15 @@ void ComputeImage(guchar *img_src, int nb_line, int nb_col, guchar *img_dst)
   for (size_t x = 1; x < nb_col-1; x++)
     for (size_t y = 1; y < nb_line-1; y++)
     {
-      size_t pos = x + y * nb_col;
+      size_t pos = (x-1) + (y-1) * nb_col;
+      printf("before %d %d\n", pos, nb_pixels);
 
       pixels[pos] = malloc(sizeof(unsigned) * 5);
 
       pixels[pos][0] = img_dst[x + y * nb_col];       // Current pixel
       pixels[pos][1] = img_dst[x + (y-1) * nb_col];   // Up
       pixels[pos][2] = img_dst[(x+1) + y * nb_col];   // Right
-      pixels[pos][3] = img_dst[(x + (y+1) * nb_col)]; // Down
+      pixels[pos][3] = img_dst[x + (y+1) * nb_col]; // Down
       pixels[pos][4] = img_dst[(x-1) + y * nb_col];   // Left
     }
 
@@ -61,7 +63,7 @@ void ComputeImage(guchar *img_src, int nb_line, int nb_col, guchar *img_dst)
   // Free pixels neighbours
   for (size_t x = 1; x < nb_col-1; x++)
     for (size_t y = 1; y < nb_line-1; y++)
-      free(pixels[x + y * nb_col]);
+      free(pixels[x + y * (nb_col-1)]);
   free(pixels);
 }
 
@@ -75,45 +77,135 @@ int** kmeans(int nb_line, int nb_col, unsigned** pixels)
   unsigned max_val = 255;
   int nb_pixels = (nb_line - 2) * (nb_col - 2);
   unsigned* classification = calloc(nb_pixels, sizeof(unsigned));
-  unsigned** centroids = malloc(sizeof(unsigned*) * nb_class);
+  unsigned* nb_per_class = calloc(nb_class, sizeof(unsigned));
+  unsigned* nb_per_class2 = calloc(nb_class, sizeof(unsigned));
+  unsigned** centers = malloc(sizeof(unsigned*) * nb_class);
+
+  unsigned** medians = malloc(sizeof(unsigned*) * nb_class);
+  for (size_t class_i = 0; class_i < nb_class; class_i++)
+    medians[class_i] = NULL;
 
   // Initialize centroids
   for (size_t class_i = 0; class_i < nb_class; class_i++)
   {
-    centroids[class_i] = malloc(sizeof(unsigned) * vec_size);
+    centers[class_i] = malloc(sizeof(unsigned) * vec_size);
     for (size_t vec_i = 0; vec_i < vec_size; vec_i++)
-      centroids[class_i][vec_i] = (rand() % (max_val + 1 - min_val)) + min_val;
+      centers[class_i][vec_i] = (rand() % (max_val + 1 - min_val)) + min_val;
   }
 
+  int c = 0;
   while (1)
   {
-    // Classify pixels per centroids
+    printf("Loop %d\n", c);
+    if (c > 5)
+      break;
+    c++;
+    // -----------------------------------
+    // 1. Classify pixels per mass centers
+    printf("Classifying...\n");
     for (size_t x = 1; x < nb_col-1; x++)
       for (size_t y = 1; y < nb_line-1; y++)
       {
-        size_t pos = x + y * nb_col;
+        size_t pos = (x-1) + (y-1) * nb_col;
 
         size_t best_class = 0;
         double min_dist = DBL_MAX;
         for (size_t class_i = 0; class_i < nb_class; class_i++)
         {
           double dist = cartesian_distance(pixels[pos],
-                                           centroids[class_i],
+                                           centers[class_i],
                                            vec_size);
           if (dist < min_dist)
           {
             min_dist = dist;
             best_class = class_i;
 
-            if (!min_dist) // Cannot be closer to centroid
+            if (!min_dist) // Cannot be closer to a mass center
               break;
           }
         }
 
+        printf("toto %d %d\n", pos, nb_pixels);
+        nb_per_class[best_class]++;
+        nb_per_class2[best_class]++;
         classification[pos] = best_class;
       }
 
+    // -----------------------------------
+    // 2. Compute new mass centers
+    printf("Computing mass centers...\n");
+    for (size_t class_i = 0; class_i < nb_class; class_i++)
+    {
+      free(medians[class_i]);
+      medians[class_i] = calloc(nb_per_class[class_i], sizeof(unsigned));
+    }
+    // Aglomerate pixels belonging to each class
+    for (size_t x = 1; x < nb_col-1; x++)
+      for (size_t y = 1; y < nb_line-1; y++)
+      {
+        size_t pos = (x-1) + (y-1) * nb_col;
+        size_t class = classification[pos];
+
+        medians[class][nb_per_class[class]-1] = pixels[pos];
+        nb_per_class[class] -= 1;
+      }
+
+    // Sort each group of pixels
+    for (size_t class_i = 0; class_i < nb_class; class_i++)
+    {
+      sort(medians[class_i], nb_per_class2[class_i]);
+
+      unsigned new_value;
+      if (nb_per_class2[class_i] % 2 != 0) // Odd
+        new_value = medians[class_i][nb_per_class2[class_i] / 2];
+      else // Even
+        new_value = (medians[class_i][nb_per_class2[class_i] / 2] +\
+                     medians[class_i][nb_per_class2[class_i] / 2 + 1]) / 2;
+
+      memset(centers[class_i], new_value, vec_size);
+
+    }
+
+    memset(nb_per_class, 0, nb_class);
+    memset(nb_per_class2, 0, nb_class);
   }
+
+  free(nb_per_class);
+  free(nb_per_class2);
+  free(classification);
+  for (size_t class_i = 0; class_i < nb_class; class_i++)
+  {
+    free(centers[class_i]);
+    free(medians[class_i]);
+  }
+  free(centers);
+  free(medians);
+}
+
+void sort(unsigned* values, unsigned len)
+{
+  if (len < 2)
+    return;
+
+  int pivot = values[len / 2];
+
+  int i, j;
+  for (i = 0, j = len - 1; ; i++, j--) {
+    while (values[i] < pivot)
+      i++;
+    while (values[j] > pivot)
+      j--;
+
+    if (i >= j)
+      break;
+
+    int temp = values[i];
+    values[i]     = values[j];
+    values[j]     = temp;
+  }
+
+  sort(values, i);
+  sort(values + i, len - i);
 }
 
 double cartesian_distance(unsigned* vec1, unsigned* vec2, size_t vec_size)
--- NUAGES_5A/SRC/compute.h
+++ NUAGES_5A/SRC/compute.h
@@ -7,3 +7,4 @@ void grayscale(int nb_line, int nb_col, guchar* img_src, guchar* img_dst);
 void ComputeImage(guchar *img_src, int nb_col, int nb_line, guchar *img_dst);
 int** kmeans(int nb_line, int nb_col, unsigned** pixels);
 double cartesian_distance(unsigned* vec1, unsigned* vec2, size_t vec_size);
+void sort(unsigned* values, unsigned len);
