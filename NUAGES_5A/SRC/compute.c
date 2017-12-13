0bdddbb6db693146040ebb5b58aeb87df6b7a5ae
Arthur Douillard <douill_a@epita.fr>
2017-12-13 10:06:41 +0100

    Add images.

---
+++ NUAGES_5A/SRC/compute.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <gtk/gtk.h>
+
+/*******************************************************
+IL EST FORMELLEMENT INTERDIT DE CHANGER LE PROTOTYPE
+DES FONCTIONS
+*******************************************************/
+
+
+/*---------------------------------------
+  Proto: 
+
+  
+  But: 
+
+  Entrees: 
+          --->le tableau des valeurs des pixels de l'image d'origine
+	      (les lignes sont mises les unes à la suite des autres)
+	  --->le nombre de lignes de l'image,
+	  --->le nombre de colonnes de l'image,
+          --->le tableau des valeurs des pixels de l'image resultat
+	      (les lignes sont mises les unes à la suite des autres)
+
+
+  Sortie:
+
+  Rem: 
+
+  Voir aussi:
+
+  ---------------------------------------*/
+void ComputeImage(guchar *pucImaOrig, 
+		  int NbLine,
+		  int NbCol, 
+		  guchar *pucImaRes)
+{
+  int iNbPixelsTotal, iNumPix;
+  int iNumChannel, iNbChannels=3; /* on travaille sur des images couleurs*/
+  guchar ucMeanPix;
+
+  printf("Segmentation de l'image.... A vous!\n");
+  
+  iNbPixelsTotal=NbCol*NbLine;
+  for(iNumPix=0;
+      iNumPix<iNbPixelsTotal*iNbChannels;
+      iNumPix=iNumPix+iNbChannels){
+    /*moyenne sur les composantes RVB */
+    ucMeanPix=(unsigned char)
+	((
+	  *(pucImaOrig+iNumPix) +
+	  *(pucImaOrig+iNumPix+1) +
+	  *(pucImaOrig+iNumPix+2))/3);
+    /* sauvegarde du resultat */
+    for(iNumChannel=0;
+	iNumChannel<iNbChannels;
+	iNumChannel++)
+      *(pucImaRes+iNumPix+iNumChannel)= ucMeanPix;
+  }
+}
+
